a87b80085b18758d44a2bc60e9905c8746353ce9
Kirti Raj <kirpurva@kirpurva.localdomain>
2026-06-14 14:34:08 +0530

    Fixed UI inconsistencies, app not responsive after video load

--- mainwindow.h
+++ mainwindow.h
@@ -1,56 +1,125 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+
 #include <QMainWindow>
+
 #include <QMediaPlayer>
+#include <QAudioOutput>
 #include <QVideoWidget>
+
 #include <QSlider>
-#include <QTimer>  
-#include <QPushButton> 
+#include <QKeyEvent>
+#include <QStackedLayout>
+#include <QPushButton>
+#include <QSlider>
+#include <QLabel>
+#include <QTimer>
+#include <QStackedLayout>
+#include <QEvent>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+
+namespace Ui
+{
+    class MainWindow;
+}
+
 QT_END_NAMESPACE
+
+
 
 class MainWindow : public QMainWindow
 {
+
     Q_OBJECT
 
+
 public:
+
     explicit MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
+
+
 protected:
+
     void keyPressEvent(QKeyEvent *event) override;
-    bool eventFilter(QObject *obj, QEvent *event) override; 
+    bool eventFilter(QObject *object, QEvent *event) override;
+
 
 private slots:
-    void openFile(); 
-    void play();     
-    void pause();    
-    void stop();     
-    void setPosition(int position); 
-    void updatePosition(qint64 position); 
-    void updateDuration(qint64 duration); 
-    void toggleFullScreen(); 
-    void hideControls();    
-    void showControls();     
+
+
+    // File
+
+    void openFile();
+
+
+
+    // Playback
+
+    void togglePlayback();
+
+
+
+    // Controls
+
+    void setPosition(int position);
+
+    void updatePosition(qint64 position);
+
+    void updateDuration(qint64 duration);
+
+
+
+    // Window
+
+    void toggleFullScreen();
+
+
 
 private:
-    Ui::MainWindow *ui; 
-    QMediaPlayer *mediaPlayer; 
-    QVideoWidget *videoWidget; 
-    QTimer *controlHideTimer;  
-    bool isPlaying;            
-    bool isFullScreen;         
 
-   
-    QWidget *fullscreenControlsOverlay;  
-    QPushButton *fsPlayButton;          
-    QPushButton *fsPauseButton;        
-    QPushButton *fsStopButton;          
-    QSlider *fsPositionSlider;          
-    QPushButton *fsExitFullscreenButton; 
+
+    Ui::MainWindow *ui;
+
+
+    // Media engine
+
+    QMediaPlayer *mediaPlayer;
+
+    QAudioOutput *audioOutput;
+
+    QVideoWidget *videoWidget;
+
+    QStackedLayout *overlayLayout;
+
+    // Controls
+
+    QWidget *controlsOverlay;
+
+    QPushButton *openButton;
+    QPushButton *playButton;
+    QPushButton *fullscreenButton;
+
+    QSlider *positionSlider;
+    QSlider *volumeSlider;
+
+    QLabel *currentTimeLabel;
+    QLabel *totalTimeLabel;
+
+    QTimer *hideTimer;
+
+    // States
+
+    bool isPlaying;
+
+    bool isFullScreen;
+
 };
 
-#endif 
+
+#endif
+
